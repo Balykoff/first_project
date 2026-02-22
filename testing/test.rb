40ea1d4ff9a575bbba27671f5f28c3ad51a19649
user1 <Balykoff@gmail.com>
2026-02-22 14:00:42 +0300

    modified test.rb

--- testing/test.rb
+++ testing/test.rb
@@ -1,2 +1,10 @@
 <!DOCTYPE html>
-<html>
+<html lang="eng">
+<head>
+    <title>Invalid HTML Example</title>
+</head>
+<body>
+    <h1>This is a test file</h1>
+    <p>This paragraph is missing its closing tag.
+</body>
+</html>
