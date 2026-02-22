03fbf24d669b795d8ceedc78a1abd547aab1a769
user1 <Balykoff@gmail.com>
2026-02-22 11:16:11 +0300

    fix test.rb

--- testing/test.rb
+++ testing/test.rb
@@ -1,2 +1,14 @@
 <!DOCTYPE html>
-<html lang="eng">
+<html>
+	<head>
+		<title>Тестовая страница</title>
+	</head>
+<body>
+	<header>
+		<h1>Первый тест</h1>
+	</header>
+	<h2>Привет HTML и JavaScript</h2>
+	<p>После этой строки будем писать контент и вставлять тестируемый код</p>
+	
+</body>
+</html
