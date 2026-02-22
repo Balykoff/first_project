97b0e4cea3a6844280ce18fc358318c4431be712
Dave Bulk <davidbaaron@icloud.com>
2026-02-22 05:23:10 +0200

    modified test.rb and add test.js

--- testing/test.rb
+++ testing/test.rb
@@ -1,2 +1,12 @@
 <!DOCTYPE html>
+<html>
+<head>
+    <title>Invalid HTML Example</title>
+</head>
+<body>
+    <h1>This is a test file</h1>
+    <p>This paragraph is missing its closing tag.
+</body>
+</html>
+<!DOCTYPE html>
 <html lang="eng">
