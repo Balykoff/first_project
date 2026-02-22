63305a5714a392b17cb91938e726b1ff99865d64
user1 <Balykoff@gmail.com>
2026-02-22 13:34:18 +0300

    bsck to previous virsion test.rb file

--- testing/test.rb
+++ testing/test.rb
@@ -1,27 +1,2 @@
 <!DOCTYPE html>
 <html>
-<<<<<<< HEAD
-<head>
-    <title>Invalid HTML Example</title>
-</head>
-<body>
-    <h1>This is a test file</h1>
-    <p>This paragraph is missing its closing tag.
-</body>
-</html>
-<!DOCTYPE html>
-<html lang="eng">
-=======
-	<head>
-		<title>Тестовая страница</title>
-	</head>
-<body>
-	<header>
-		<h1>Первый тест</h1>
-	</header>
-	<h2>Привет HTML и JavaScript</h2>
-	<p>После этой строки будем писать контент и вставлять тестируемый код</p>
-	
-</body>
-</html
->>>>>>> hotfix/fix-test-bag
